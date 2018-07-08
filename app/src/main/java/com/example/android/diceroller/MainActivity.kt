15318417f834bf44cd5f6caabac09918cb55d91d
Jose Nieto <dnbit@users.noreply.github.com>
2018-07-08 19:13:05 +0100

    Step.04-Solution-Adding-the-ImageView

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,7 +18,7 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -36,13 +36,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        // TODO (04) Choose the right drawable resource based on the value of randomInt
-        // Tip: Use a when expression
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        // TODO (03) Get a reference to the imageView instead
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        // TODO (05) Make the ImageView show the chosen drawable resource
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
