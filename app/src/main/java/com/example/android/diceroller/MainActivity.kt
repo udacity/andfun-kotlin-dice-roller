8e687154ce870860222b7a0ea625eb7ba4d70a00
Jose Nieto <dnbit@users.noreply.github.com>
2018-07-11 14:40:27 +0100

    Step.02-Exercise-OnClickListener

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -24,5 +24,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO (02) Use findViewById to get a reference to the button
+        // and assign it to an immutable variable called rollButton
+
+        // TODO (03) set the OnClickListener for the button
+
+        // TODO (04) Show a Toast when the OnClickListener is called
+
     }
+
 }
