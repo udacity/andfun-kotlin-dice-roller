5361fafd69cbe7f292ceea621ffed275ee6bac34
Lyla M. Fujiwara <lmf@google.com>
2018-11-05 17:32:32 -0800

    Step.03-Solution-Rolling-the-dice

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,8 +18,9 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,20 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            // TODO (02) Remove the Toast and instead create a method called rollDice here. You can
-            // use the keyboard shortcut:
-            //    Windows/Linux - Alt + Enter
-            //    Mac - Option + Enter
-
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
 
-        // TODO (03) In rollDice get a Random int between 1 and 6
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
 
-        // TODO (04) In rollDice use findViewById to get a reference to the TextView
-        // and assign it to an inmutable variable called resultText
-
-        // TODO (05) In rollDice set the random value that you got above as the
-        // text of the TextView
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
